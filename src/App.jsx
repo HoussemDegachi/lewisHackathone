f042d0a7f0559e3c1dd277dd63f97f68a34b08a6
HoussemDegachi <houssemdegach@gmail.com>
2024-08-11 21:00:37 +0200

    Fixed .jsx problem (i am sure)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import Editor from "@/pages/editor/index"
+import Editor from "@/pages/editor/index.jsx"
 
 function App() {
 
