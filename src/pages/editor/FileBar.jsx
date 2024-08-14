f349f513fbc0166f217cb99e792563028531f842
HoussemDegachi <houssemdegach@gmail.com>
2024-08-14 21:01:53 +0200

    recoverd filebar

--- src/pages/editor/FileBar.jsx
+++ src/pages/editor/FileBar.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import FileList from "./FileBarCompoents/FileList";
 import { useFileBarDataProvider } from "@/contexts/FileBarDataProvider";
 import SavePageButton from "@/components/ui/saveButton";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 
 function FileBar() {
   const { directory } = useFileBarDataProvider();
-  // console.dir(directory);
 
   return (
     <div className="w-full h-full flex flex-col text-white bg-slate-900">
+      <ResizablePanelGroup direction={"vertical"}>
+        <ResizablePanel defaultSize={80}>
       <h2 className="border-gray-700 font-medium border-b-2 p-1.5">EXPLORER</h2>
-      <SavePageButton />
       <div className="p-1 select-none">
         <FileList file={directory} />
       </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle={false} className={"bg-neutral-400"} />
+        <ResizablePanel defaultSize={20} className="max-h-[50%]">
+          <TimeLine /> 
+        </ResizablePanel>
+    </ResizablePanelGroup>
     </div>
   );
 }
