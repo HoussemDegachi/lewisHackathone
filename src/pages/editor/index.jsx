d01dd3860427d3582b70de27941f1f57e5a37877
HoussemDegachi <houssemdegach@gmail.com>
2024-08-14 21:00:50 +0200

    .

--- src/pages/editor/index.jsx
+++ src/pages/editor/index.jsx
@@ -2,8 +2,6 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/componen
 import FileBar from "./FileBar.jsx";
 import EditorSection from "./EditorSection.jsx";
 import NoFile from "./EditorSectionComponents/NoFile.jsx";
-import { FileBarDataProvider } from "@/contexts/FileBarDataProvider.jsx";
-import { EditorDataProvider } from "@/contexts/EditorDataProvider.jsx";
 
 
 function index({ isRoot }) {
@@ -12,10 +10,9 @@ function index({ isRoot }) {
     // create a resizable panel with two sides
     <ResizablePanelGroup
     direction="horizontal"
-    className="w-full min-h-[100dvh] max-h-[100dvh]"
-  >
+    className="w-full min-h-[100dvh] max-h-[100dvh]">
       {/* small side is for file bar */}
-      <ResizablePanel defaultSize={20}>
+      <ResizablePanel defaultSize={20} className="min-w-[180px] max-w-[300px]">
         <div className="h-full">
             <FileBar />
         </div>
@@ -24,11 +21,9 @@ function index({ isRoot }) {
       {/* large side is for code editor section */}
       <ResizablePanel defaultSize={80}>
         <div className="h-full">
-        <EditorDataProvider>
           {
             isRoot ? <NoFile /> : <EditorSection />
           }
-        </EditorDataProvider>
         </div>
       </ResizablePanel>
     </ResizablePanelGroup>
