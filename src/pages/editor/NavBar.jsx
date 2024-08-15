0ab46fd70af410f902e89bd60e6dd9deb480d98c
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-08-15 13:23:36 +0530

    Major refactoring and improvements.

--- src/pages/editor/NavBar.jsx
+++ src/pages/editor/NavBar.jsx
@@ -1,45 +1,21 @@
-import JSZip from "jszip";
 import { useParams } from "react-router-dom";
 import { Braces, FileDown, FolderDown } from "lucide-react";
 import { useFileBarDataProvider } from "@/contexts/FileBarDataProvider";
 import { saveAs } from "file-saver";
+import { downloadFile, downloadFolder } from "@/lib/downloadOps";
 
 function NavBar() {
   const { fileId } = useParams();
   const file = JSON.parse(localStorage.getItem(fileId));
-  console.log(file);
 
   const { directory } = useFileBarDataProvider();
 
-  const downloadFile = (fileId) => {
-    const file = JSON.parse(localStorage.getItem(fileId));
-    if (file && file.language && file.content) {
-      const blob = new Blob([file.content]);
-      saveAs(blob, file.fullName);
-    }
+  const handleFileDownload = () => {
+    downloadFile(fileId);
   };
 
-  const downloadProject = (directory) => {
-    function createZip(directory, folder = new JSZip().folder(directory.name)) {
-      if (directory.type === "file") {
-        const file = JSON.parse(localStorage.getItem(directory.id));
-        console.log(file);
-
-        folder.file(file.fullName, file.content);
-      }
-
-      if (directory.type === "folder") {
-        let subfolder = folder.folder(directory.name);
-        if (directory.contents)
-          for (const content of directory.contents)
-            createZip(content, subfolder);
-      }
-      return folder;
-    }
-    let zip = createZip(directory);
-    zip.generateAsync({ type: "blob" }).then(function (blob) {
-      saveAs(blob, `${directory.name}.zip`);
-    });
+  const handleProjectDownload = () => {
+    downloadFolder(directory);
   };
 
   return (
@@ -49,19 +25,21 @@ function NavBar() {
         Chaos Code Editor
       </h1>
       <div className="mr-5 flex items-center gap-2">
+        {file && (
+          <button
+            onClick={handleFileDownload}
+            className="bg-gray-700 py-1.5 px-3 h-10 rounded-md group flex items-center text-sm"
+          >
+            <FileDown size={22} />
+            {file?.fullName && (
+              <span className="max-w-0 group-hover:max-w-28 group-hover:pl-1 whitespace-nowrap overflow-hidden transition-all duration-500">
+                {file.fullName}
+              </span>
+            )}
+          </button>
+        )}
         <button
-          onClick={() => downloadFile(fileId)}
-          className="bg-gray-700 py-1.5 px-3 h-10 rounded-md group flex items-center text-sm"
-        >
-          <FileDown size={22} />
-          {file?.fullName && (
-            <span className="max-w-0 group-hover:max-w-28 group-hover:pl-1 whitespace-nowrap overflow-hidden transition-all duration-500">
-              {file.fullName}
-            </span>
-          )}
-        </button>
-        <button
-          onClick={() => downloadProject(directory)}
+          onClick={handleProjectDownload}
           className="bg-gray-700 py-1.5 px-3 h-10 rounded-md group flex items-center text-sm"
         >
           <FolderDown size={22} />
