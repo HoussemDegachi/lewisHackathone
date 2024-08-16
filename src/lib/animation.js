776af5511f7b6f2a280d55e086134a2f1b587d2b
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-08-16 16:50:18 +0530

    Optimised runaway button animations

--- src/lib/animation.js
+++ src/lib/animation.js
@@ -5,14 +5,14 @@ export function moveElement(element, animeType, pixels) {
   anime({
     targets: element,
     [animeType]: `${pixels}px`,
-    easing: "easeOutElastic(1, .5)",
-    duration: 1000,
+    easing: "easeOutElastic(1, 1)",
+    duration: 2300,
   }).play();
 }
 
 export function changePosition(button, type) {
   button.addEventListener(type, function () {
-    const top = getRandomNumber(75, window.innerHeight - this.offsetHeight);
+    const top = getRandomNumber(0, window.innerHeight - this.offsetHeight);
     const left = getRandomNumber(75, window.innerWidth - this.offsetWidth);
 
     moveElement(this, "left", left);
