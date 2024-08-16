d2c33db17f0d396e1023dce5133ec56e3232d12b
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-08-16 16:11:50 +0530

    Fixed font.

--- src/pages/editor/EditorSectionComponents/NoFile.jsx
+++ src/pages/editor/EditorSectionComponents/NoFile.jsx
@@ -3,9 +3,10 @@ import React from "react";
 function NoFile() {
   return (
     <div className="w-full h-full bg-editor flex flex-col items-center justify-center">
-      <p className="text-white font-bold text-xl text-center font-sans">
-        {" "}
-        The void remains empty. <br /> <span className="block text-sm font-sans">Add some chaos!</span>
+      <p className="text-white font-bold text-xl text-center">
+        The void remains empty.
+        <br />
+        <span className="block text-sm">Add some chaos!</span>
       </p>
     </div>
   );
